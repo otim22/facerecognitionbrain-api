35a7b05d9703fd9ca6931431109f2a65fa395e7a
Fredrick Otim <fredrickot@gmail.com>
2018-08-11 19:15:31 +0300

    project setup files

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('This is working');
+})
+
+app.listen(3000, () => {
+    console.log('App is running! on port 3000')
+})
