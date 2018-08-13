c9286248c3e28bfdd4229b62f2bbcaf2499a6c51
Fredrick Otim <fredrickot@gmail.com>
2018-08-13 22:32:01 +0300

    adds user password storage

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
 
 const app = express();
 
@@ -40,6 +41,9 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
+    bcrypt.hash(password, null, null, function(err, hash) {
+    console.log(hash);
+    });
     database.users.push({
         id: '125',
         name: name,
@@ -79,6 +83,14 @@ app.post('/image', (req, res) => {
         res.status(400).json('Not found');
     }
 })
+
+// // Load hash from your password DB.
+// bcrypt.compare("bacon", hash, function(err, res) {
+//     // res == true
+// });
+// bcrypt.compare("veggies", hash, function(err, res) {
+//     // res = false
+// });
 
 app.listen(3000, () => {
     console.log('App is running! on port 3000')
