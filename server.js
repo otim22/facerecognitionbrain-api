e76b30889a5f0543d844c3ff5db125dcf55649eb
Fredrick Otim <fredrickot@gmail.com>
2018-08-14 22:17:39 +0300

    updates success text

--- server.js
+++ server.js
@@ -35,7 +35,8 @@ app.get('/', (req, res) => {
 app.post('/signin', (req, res) => {
     if (req.body.email === database.users[0].email &&
         req.body.password === database.users[0].password) {
-        res.json('success');
+        // res.json('success');
+        res.json(database.users[0]);
     } else {
         res.status(400).json('error logging in')
     }
