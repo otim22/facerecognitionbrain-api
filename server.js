60a9e93e4c5879f350434ecaf81880415491b510
Fredrick Otim <fredrickot@gmail.com>
2018-09-10 11:53:15 +0300

    heroku database setting

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 var dbConnect = require('knex')({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-octagonal-33165',
     user : 'fredrickotim',
     password : '',
     database : 'smart-brain'
