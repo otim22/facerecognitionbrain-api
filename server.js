1f10cb7c5d05728708c4b03b940039c4f920b4e7
Fredrick Otim <fredrickot@gmail.com>
2018-09-10 12:11:09 +0300

    fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 var dbConnect = require('knex')({
   client: 'pg',
   connection: {
-    host : process.env.DATABASE_URL,
+    connectionString : process.env.DATABASE_URL,
     ssl: true,
   }
 });
