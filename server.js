e57bd8f73ca531891637716a41942ad4974bf13c
Fredrick Otim <fredrickot@gmail.com>
2018-09-10 11:29:21 +0300

    adding port

--- server.js
+++ server.js
@@ -47,5 +47,5 @@ app.post('/imageurl', (req, res) => {
 })
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('App is running! on port ${ process.env.PORT }')
+    console.log(`App is running! on port ${process.env.PORT}`)
 })
